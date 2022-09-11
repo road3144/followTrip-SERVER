24cfd6eba5c0500d598a3f816b1238aeb2b27277
road3144 <fnfnfn258@gmail.com>
2022-09-11 14:19:44 +0900

    initial commit

---
+++ src/main/java/com/road3144/followtrip/FollowTripApplication.java
@@ -0,0 +1,13 @@
+package com.road3144.followtrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollowTripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FollowTripApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/road3144/followtrip/FollowTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.road3144.followtrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FollowTripApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
